59792bf65213b443f153bc4796f987a706c49f26
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-11-30 19:18:29 +0600

    register func added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCC3ahM2O8CgugYyWuESZN4-10_cO9tDOo",
+  authDomain: "dragon-news-58d01.firebaseapp.com",
+  projectId: "dragon-news-58d01",
+  storageBucket: "dragon-news-58d01.firebasestorage.app",
+  messagingSenderId: "833815186275",
+  appId: "1:833815186275:web:f7be0db12df75d9d9c2156"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
